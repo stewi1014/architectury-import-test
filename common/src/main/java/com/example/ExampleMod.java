ec0461e0bd152ff8c33bb7361a54fdf8b32c902a
Stewart Borle <stewi1014@gmail.com>
2024-11-19 21:13:19 +0800

    Template

---
+++ common/src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "example_mod";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
