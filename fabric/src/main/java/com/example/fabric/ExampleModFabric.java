d8f3e80c73ca05aa8e236db8e22634c51f6c3853
Stewart Borle <stewi1014@gmail.com>
2024-11-19 21:38:09 +0800

    Example import problem

--- fabric/src/main/java/com/example/fabric/ExampleModFabric.java
+++ fabric/src/main/java/com/example/fabric/ExampleModFabric.java
@@ -3,13 +3,22 @@ package com.example.fabric;
 import net.fabricmc.api.ModInitializer;
 
 import com.example.ExampleMod;
+import org.apache.sshd.common.NamedResource;
+import org.eclipse.jgit.gitrepo.RepoCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class ExampleModFabric implements ModInitializer {
+    private static final Logger log = LoggerFactory.getLogger(ExampleModFabric.class);
+
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
+
+        log.info(RepoCommand.class.getTypeName()); // works
+        log.info(NamedResource.class.getTypeName()); // works
 
         // Run our common setup.
         ExampleMod.init();
--- neoforge/src/main/java/com/example/neoforge/ExampleModNeoForge.java
+++ neoforge/src/main/java/com/example/neoforge/ExampleModNeoForge.java
@@ -2,11 +2,21 @@ package com.example.neoforge;
 
 import net.neoforged.fml.common.Mod;
 
+import org.eclipse.jgit.gitrepo.RepoCommand;
+import org.apache.sshd.common.NamedResource;
+
 import com.example.ExampleMod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(ExampleMod.MOD_ID)
 public final class ExampleModNeoForge {
+    private static final Logger log = LoggerFactory.getLogger(ExampleModNeoForge.class);
+
     public ExampleModNeoForge() {
+        log.info(RepoCommand.class.getTypeName()); // works
+        log.info(NamedResource.class.getTypeName()); // java.lang.NoClassDefFoundError
+
         // Run our common setup.
         ExampleMod.init();
     }
